672c373d176f44f031c0e631e009b57c54289d2d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-24 23:51:10 +0300

    added dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
+import Head from "next/head"
+import Image from "next/image"
+import axios from "axios"
+import { useState } from "react"
+import BsSearch from "react-icons/bs"
 
 export default function Home() {
+
+  const [city, setCity] = useState('')
+  const [weather, setWeather] = useState({})
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&lat={lat}&lon={lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
 
