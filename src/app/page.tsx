b56d5a3516bbda75a6c8453344ecea65546c28ea
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-24 23:09:17 +0300

    added Head for Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center text-blue-600 font-bold justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      NEW
+      <Head>
+
+      </Head>
     </div>
   );
 }
