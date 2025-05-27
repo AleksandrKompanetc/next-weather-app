53789f99cf57007cb478f789c8435c131a36eb4f
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-27 23:42:16 +0300

    added some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,10 @@ export default function Home() {
     setLoading(false)
   }
 
+  if(loading) {
+    
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center text-blue-600 font-bold justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Head>
