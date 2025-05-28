d7740737875f303bbc5013dc69dccabc4377372a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-28 23:31:05 +0300

    added Loading

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Weather = ({data}) => {
   console.log(data)
   return (
-    <div>Weather</div>
+    <div>
+      <div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
