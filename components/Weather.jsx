4c9bfcdcae8fb979a83ccc82f41f7aacfa800428
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-29 23:00:03 +0300

    changed styles

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -5,16 +5,17 @@ const Weather = ({data}) => {
   console.log(data)
   return (
     <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4'>
-      <div>
-        <div>
+      <div className='relative flex justify-between pt-12'>
+        <div className='flex flex-col items-center'>
           <Image 
             src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
             alt='/' 
             width='100'
             height='100'
           />
-          <p>{data.main.temp.toFixed(0)}& #176;</p>
+          <p className='text-2xl'>{data.weather[0].main}</p>
         </div>
+        <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
       </div>
     </div>
   )
