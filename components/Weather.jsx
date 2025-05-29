fff2fece178eb8a572c5443fec74958ab0470e20
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-29 23:13:05 +0300

    changed Weather component

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -13,9 +13,18 @@ const Weather = ({data}) => {
             width='100'
             height='100'
           />
-          <p className='text-2xl'>{data.weather[0].main}</p>
+          <p className='text-2xl text-white'>{data.weather[0].main}</p>
         </div>
-        <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
+        <p className='text-9xl text-white'>{data.main.temp.toFixed(0)}&#176;</p>
+      </div>
+
+      <div>
+        <p>Weather in {data.name}</p>
+        <div>
+          <div>
+            <p>{data.main.feels_like.toFixed(0)}&#176;</p>
+          </div>
+        </div>
       </div>
     </div>
   )
